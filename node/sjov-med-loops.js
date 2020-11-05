b3fbd8fd98dc57543e697dfdbd49c7fe7680216f
Rasmus Stougaard <rst@eucnordvest.dk>
2020-11-05 12:07:18 +0100

    Eksempler med loops i node js

---
+++ node/sjov-med-loops.js
@@ -0,0 +1,14 @@
+
+// Opg 1 tæl ned fra 10 - med while løkke
+console.log("opg 1 for loop")
+let i = 10
+while(i >= 1){
+  console.log(i);
+  i--;
+}
+
+// Opg 1 tæl ned fra 10 - med for løkke
+console.log("opg 1 for loop")
+for(let i = 10;i >= 1;i--){
+  console.log(i);
+}
---
+++ node/sjov-med-random.js
@@ -0,0 +1,7 @@
+let a = Math.floor(Math.random() * 16 + 1)
+
+console.log("tal "+a)
+
+for (let i = a; i >= 1; i--) {
+  console.log(i);
+}
