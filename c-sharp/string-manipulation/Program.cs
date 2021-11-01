01aea18badb05dd87a73ed21ce4be5a4404c063e
Rasmus Stougaard <rst@eucnordvest.dk>
2021-11-01 09:11:58 +0100

    Add string manipulation template

---
+++ c-sharp/string-manipulation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace string_manipulation
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("String manipulation");
+      Opgave1();
+      Opgave2();
+    }
+
+    static void Opgave1()
+    {
+      Console.WriteLine("### Opgave 1");
+      // do stuff: løs opgaven
+    }
+    static void Opgave2()
+    {
+      Console.WriteLine("### Opgave 2");
+      // do stuff: løs opgaven
+    }
+  }
+}
