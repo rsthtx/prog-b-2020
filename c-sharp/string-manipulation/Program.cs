669f98a7d432169bb95f0207cb508dc7556148ca
Rasmus Stougaard <rst@eucnordvest.dk>
2021-11-01 10:37:19 +0100

    Add argument to method

--- c-sharp/string-manipulation/Program.cs
+++ c-sharp/string-manipulation/Program.cs
@@ -8,7 +8,8 @@ namespace string_manipulation
     {
       Console.WriteLine("String manipulation");
       Opgave1();
-      Opgave2();
+      Opgave2("hej med dig");
+      Opgave2("Nu er der er måske snart frikvarter");
     }
 
     static void Opgave1()
@@ -16,10 +17,12 @@ namespace string_manipulation
       Console.WriteLine("### Opgave 1");
       // do stuff: løs opgaven
     }
-    static void Opgave2()
+    static void Opgave2(string text)
     {
       Console.WriteLine("### Opgave 2");
       // do stuff: løs opgaven
+
+      Console.WriteLine(text);
     }
   }
 }
